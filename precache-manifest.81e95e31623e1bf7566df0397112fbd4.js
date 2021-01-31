caccac7acb99731432b002fc63404e520d8c3527
Mikus <mikusmeikulis@gmail.com>
2021-01-31 21:36:13 +0200

    deploy

---
+++ precache-manifest.81e95e31623e1bf7566df0397112fbd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca3a5fadfbfb6874256",
+    "url": "/gerlionti/css/app.d46f454d.css"
+  },
+  {
+    "revision": "4cf20f4ea3b173a1a18f",
+    "url": "/gerlionti/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "f57f33d3cabb257a9fc9709f10eda78f",
+    "url": "/gerlionti/index.html"
+  },
+  {
+    "revision": "eca3a5fadfbfb6874256",
+    "url": "/gerlionti/js/app.655d15af.js"
+  },
+  {
+    "revision": "4cf20f4ea3b173a1a18f",
+    "url": "/gerlionti/js/chunk-vendors.a728577d.js"
+  },
+  {
+    "revision": "e551f4e728cee006ec424e8dc9b6d1ec",
+    "url": "/gerlionti/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gerlionti/robots.txt"
+  }
+]);
